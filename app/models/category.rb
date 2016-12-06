b4fd6f281753abe80fe1949072b5cef2dd34c511
Raghu Betina <raghu@firstdraft.com>
2016-12-06 20:36:55 +0000

    Validate presence of categories.refrigeration

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,6 +5,8 @@ class Category < ApplicationRecord
 
   # Validations
 
+  validates :refrigeration, :presence => true
+
   validates :storage_suggestions, :presence => true
 
   validates :type, :uniqueness => true
