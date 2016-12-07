a14be44aff3b9de8e7dc76962a3c6fcf9f40a38a
Hilary Gerstein <hilary.gerstein@users.noreply.github.com>
2016-12-07 10:49:14 -0500

    States seed
    Added states.csv and wrote code in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# categories seed
 require 'csv'
 
 csv_text = File.read(Rails.root.join('lib', 'seeds', 'categories.csv'))
@@ -23,5 +24,23 @@ csv.each do |row|
 end
 
 puts "There are now #{Category.count} rows in the categories table"
+
+# states seed
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'states.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = State.new
+  t.category_id = row['category_id']
+  t.stage1 = row['stage1']
+  t.stage2 = row['stage2']
+  t.stage3 = row['stage3']
+  t.stage4 = row['stage4']
+  t.save
+  puts "#{t.category_id} saved"
+end
+
+puts "There are now #{State.count} rows in the states table"
 
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
