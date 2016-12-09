9e4f116b361d9b7372ee86de3182c115899c22e6
Hilary Gerstein <hilary.gerstein@users.noreply.github.com>
2016-12-09 10:27:26 -0500

    Hardcoded recipe and rec logic in purchases dashboard
    Still trying to figure out ActiveRecord issue in order to update
    dynamically

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -24,7 +24,6 @@ class PurchasesController < ApplicationController
 
   def filter
     @today = Date.today.strftime("%m/%d/%Y").to_i
-    # @purchases = Purchase.all
     @recommendations = Recommendation.all
     render("purchases/dashboard.html.erb")
   end
